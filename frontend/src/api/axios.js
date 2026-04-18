9d69535bde24ce72f5f808892ebcce0d014e2267
Nachiket <nachiketdeore10@gmail.com>
2026-04-18 11:32:13 +0530

    Remove legacy frontend folder in favor of next-frontend

--- frontend/src/api/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-    baseURL: "https://quick-oh.onrender.com/api/v1", // Adjust based on your backend
-    withCredentials: true,
-});
-
-export default instance;
--- frontend/src/socket.js
+++
@@ -1,8 +0,0 @@
-import { io } from "socket.io-client";
-
-const socket = io("https://quick-oh.onrender.com", {
-    withCredentials: true,
-    autoConnect: true,
-});
-
-export default socket;
